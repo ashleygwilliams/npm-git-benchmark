ba6cae99538d2ed3c4a5bfedf3cea3397fe2eace
Ashley Williams <ashley666ashley@gmail.com>
2016-11-14 15:21:53 -0500

    initial

--- benchmark.sh
+++ benchmark.sh
@@ -4,8 +4,7 @@
 
 repeats=3
 output_folder='reports'
-libraries=('angular2' 'ember' 'react')
-tools=('npm' 'yarn')
+libraries=('angular2' 'git-angular2' 'ember' 'git-ember')
 base_dir=$PWD
 
 # Option parsing
@@ -31,56 +30,37 @@ output_folder="${base_dir}/${output_folder}"
 
 mkdir -p $output_folder
 
-echo 'Benchmarking: Yarn vs NPM'
-echo '========================'
+echo 'Benchmarking: GitHub vs NPM Registry'
+echo '===================================='
 echo 'Running' $repeats 'times per library, test results will be stored in' $output_folder 'directory'
 echo ''
 
 run_tests() {
-    for tool in "${tools[@]}"
-    do
-        avg_file_cc="${output_folder}/avg_${tool}_1.csv"
-        avg_file="${output_folder}/avg_${tool}_0.csv"
-        echo -n > $avg_file_cc
-        echo -n > $avg_file
-    done
+    avg_file_cc="${output_folder}/avg_1.csv"
+    avg_file="${output_folder}/avg_0.csv"
+    echo -n > $avg_file_cc
+    echo -n > $avg_file
 
     for library in "${libraries[@]}"
     do
         echo $library '=>';
-        for tool in "${tools[@]}"
-        do
-            run_benchmark $tool 1 $library # Cleaning cache
-            run_benchmark $tool 0 $library # Without cleaning cache
-        done
+        run_benchmark 1 $library # Cleaning cache
+        run_benchmark 0 $library # Without cleaning cache
     done
 }
 
 run_benchmark() {
-    tool=$1
-    clean_cache=$2
-    library=$3
-    directory="${base_dir}/libraries/${library}"
-    output_file="${output_folder}/${tool}_${clean_cache}_${library}.csv"
-    avg_file="${output_folder}/avg_${tool}_${clean_cache}.csv"
+    clean_cache=$1
+    library=$2
+    directory="${base_dir}/libraries/${library}/"
+    node_modules="${base_dir}/libraries/${library}/node_modules"
+    output_file="${output_folder}/${clean_cache}_${library}.csv"
+    avg_file="${output_folder}/avg_${clean_cache}.csv"
 
     echo -n > $output_file
 
-    case "$tool" in
-        yarn)
-            command_to_run='yarn install'
-            command_to_clear_cache='yarn cache clean'
-            ;;
-        npm)
-            command_to_run='npm install --cache-min 999999'
-            command_to_clear_cache='npm cache clean'
-            ;;
-        *)
-            exit 1
-            ;;
-    esac
-
-    cd $directory
+    command_to_run='npm install --cache-min 999999'
+    command_to_clear_cache='npm cache clean'
 
     if [ $clean_cache = 1 ]; then
         cache_text='with clean cache'
@@ -88,8 +68,7 @@ run_benchmark() {
         cache_text=''
 
         # Install once to generate cache
-        rm -rf node_modules
-        rm -f yarn.lock
+        rm -rf $node_modules
         $command_to_run > /dev/null 2>&1
     fi
 
@@ -99,7 +78,6 @@ run_benchmark() {
     for (( i = 1; i <= $repeats ; i++ ))
     do
         rm -rf node_modules
-        rm -f yarn.lock
 
         # Clean cache
         if [ $clean_cache = 1 ]; then
@@ -108,7 +86,7 @@ run_benchmark() {
 
         # runs time function for the called script, output in a comma seperated
         # format output file specified with -o command and -a specifies append
-        /usr/bin/time -f "%e %U %S" -o ${output_file} -a ${command_to_run} > /dev/null 2>&1
+        /usr/bin/time -f "%e %U %S" -o ${output_file} -a ${command_to_run} ${directory}
 
         # percentage completion
         p=$(( $i * 100 / $repeats))
@@ -122,36 +100,31 @@ run_benchmark() {
 
     avg=$(awk '{ total += $1; count++ } END { print total/count }' $output_file)
     echo -n $avg' ' >> $avg_file
-
-    cd $base_dir
 }
 
 show_results() {
     all_file=${output_folder}/avg.csv
     echo -n > $all_file
 
-    for tool in "${tools[@]}"
-    do
-        avg_file_cc="${output_folder}/avg_${tool}_1.csv"
-        avg_file="${output_folder}/avg_${tool}_0.csv"
+    avg_file_cc="${output_folder}/avg_1.csv"
+    avg_file="${output_folder}/avg_0.csv"
 
-        echo -n $tool'_with_empty_cache ' >> $all_file
-        cat $avg_file_cc >> $all_file
-        echo >> $all_file
-        echo -n $tool'_with_all_cached ' >> $all_file
-        cat $avg_file >> $all_file
-        echo >> $all_file
-    done
+    echo -n '_with_empty_cache ' >> $all_file
+    cat $avg_file_cc >> $all_file
+    echo >> $all_file
+    echo -n '_with_all_cached ' >> $all_file
+    cat $avg_file >> $all_file
+    echo >> $all_file
 
     echo ''
-    echo ' ----------------------------------------------------------------------- '
-    echo ' -------------------------- RESULTS (seconds) -------------------------- '
-    echo ' ----------------------------------------------------------------------- '
+    echo ' ----------------------------------------------------------------------------------- '
+    echo ' -------------------------------- RESULTS (seconds) -------------------------------- '
+    echo ' ----------------------------------------------------------------------------------- '
 
-    awk 'BEGIN {printf("| %24s | %12s | %12s | %12s | \n" , " ", "'${libraries[0]}'", "'${libraries[1]}'", "'${libraries[2]}'")}
-        {printf("| %24s | %12.3f | %12.3f | %12.3f | \n", $1, $2, $3, $4)}' $all_file
+    awk 'BEGIN {printf("| %24s | %12s | %12s | %12s | %12s | \n" , " ", "'${libraries[0]}'", "'${libraries[1]}'", "'${libraries[2]}'", "'${libraries[3]}'")}
+        {printf("| %24s | %12.3f | %12.3f | %12.3f | %12.3f | \n", $1, $2, $3, $4, $5)}' $all_file
 
-    echo ' ----------------------------------------------------------------------- '
+    echo ' ----------------------------------------------------------------------------------- '
 }
 
 run_tests
